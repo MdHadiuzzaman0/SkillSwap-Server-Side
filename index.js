6e5a75b1b1a1465d6fdd4d0693c40b314a9eb6a3
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-06-17 23:54:42 +0600

    feat: setup express server and mongodb connection

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = process.env.MONGODB_URL;
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
